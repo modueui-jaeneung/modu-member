f578c6754ca9fbabbb6d686a2e44be26cfa5b776
dlrbcnvk <dlrbcnvk@naver.com>
2023-10-06 12:20:57 +0900

    MemberController prefix /member

--- src/main/java/com/modu/MemberServer/controller/MemberController.java
+++ src/main/java/com/modu/MemberServer/controller/MemberController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@RestController
+@RestController("/member")
 @RequiredArgsConstructor
 public class MemberController {
 
