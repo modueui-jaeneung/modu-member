dcf397c9cd0601b8d3fcb6fcbfa1fe19bee07745
dlrbcnvk <dlrbcnvk@naver.com>
2023-10-09 17:04:10 +0900

    prefix /member for k8s

--- src/main/java/com/modu/MemberServer/controller/MemberController.java
+++ src/main/java/com/modu/MemberServer/controller/MemberController.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-//@RequestMapping("/member")
+@RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
 
